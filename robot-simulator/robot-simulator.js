98ebec5aa253da605a74bfa5a99950f30fa6a30e
Mila Klimovich <mila.k.klimovich@gmail.com>
2024-04-11 15:24:03 +0800

    project complete

--- robot-simulator/robot-simulator.js
+++ robot-simulator/robot-simulator.js
@@ -1,4 +1,3 @@
-
 export class InvalidInputError extends Error {
   constructor(message) {
     super();
@@ -28,6 +27,44 @@ export class Robot {
   }
 
   evaluate(instructions) {
-    throw new Error('Remove this statement and implement this function');
+    const instructionsArr = instructions.split('');
+
+    instructionsArr.forEach((i) => {
+      switch (i) {
+        case 'R':
+          if (this.dir.indexOf(this.bearing) < 3) {
+            this.bearing = this.dir[this.dir.indexOf(this.bearing) + 1];
+          } else if (this.dir.indexOf(this.bearing) === 3) {
+            this.bearing = 'north';
+          }
+          break;
+        case 'L':
+          if (this.dir.indexOf(this.bearing) > 0) {
+            this.bearing = this.dir[this.dir.indexOf(this.bearing) - 1];
+          } else if (this.dir.indexOf(this.bearing) === 0) {
+            this.bearing = 'west';
+          }
+          break;
+        case 'A':
+          switch (this.bearing) {
+            case 'north':
+              this.y += 1;
+              break;
+            case 'south':
+              this.y -= 1;
+              break;
+            case 'east':
+              this.x += 1;
+              break;
+            case 'west':
+              this.x -= 1;
+              break;
+            default:
+              break;
+          }
+          break;
+        default:
+      }
+    });
   }
 }
